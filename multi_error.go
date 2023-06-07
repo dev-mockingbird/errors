7b3ad18ef0533574e90de5c8886f8369d4fff2ea
yang,zhong <yang-zhong@udious.com>
2023-06-07 23:37:02 +0800

    complete multi error

--- multi_error.go
+++ multi_error.go
@@ -1,27 +1,49 @@
 package errors
 
-import "strings"
+import (
+	"strings"
+	"sync"
+)
 
 type JoinableError interface {
 	Join(err error)
+	AsyncJoin(err error)
+	Has() int
 	Error() string
 }
 
-type multiError []error
+type multiError struct {
+	errors []error
+	lock   sync.RWMutex
+}
 
-var _ error = multiError{}
+var _ error = &multiError{}
 
 func MultiError() JoinableError {
 	return &multiError{}
 }
 
 func (me *multiError) Join(err error) {
-	*me = append(*me, err)
+	me.errors = append(me.errors, err)
 }
 
-func (me multiError) Error() string {
-	errs := make([]string, len(me))
-	for i, e := range me {
+func (me *multiError) AsyncJoin(err error) {
+	me.lock.Lock()
+	defer me.lock.Unlock()
+	me.errors = append(me.errors, err)
+}
+
+func (me *multiError) Has() int {
+	me.lock.RLock()
+	defer me.lock.RUnlock()
+	return len(me.errors)
+}
+
+func (me *multiError) Error() string {
+	me.lock.RLock()
+	defer me.lock.RUnlock()
+	errs := make([]string, len(me.errors))
+	for i, e := range me.errors {
 		errs[i] = e.Error()
 	}
 	return strings.Join(errs, "\n")
